5e81dd732320476cac236b5ab4403808cc7c978b
Caprice-Instinct <linetwangui04@gmail.com>
2024-10-14 17:28:57 +0300

    option modes added, endless mode, best to 3

--- main.py
+++ main.py
@@ -7,37 +7,78 @@ AI_score = 0
 
 prompt = "Rock, paper, scissors... "
 
-while player_score < 3 and AI_score < 3:
-    player_input = input(prompt).title()
-    a = get_AI_guess()
-    print(f"AI chose {a}.")
+option_modes = ['Endless', 'Best to 3']
+print("Which mode would you like to play: ")
+for index, mode in enumerate(option_modes):
+    print(f"{index + 1}. {mode}.")
 
-    if a == player_input:
-        print("We are so similar! We tied")
-    elif player_input == options[0]:
-        if a == options[2]:
-            player_score += 1
-            print(f"{player_input} smashes {a}.")
+user_mode = int(input())
+
+if option_modes[user_mode-1] == option_modes[1]:
+    while player_score < 3 and AI_score < 3:
+        player_input = input(prompt).title()
+        a = get_AI_guess()
+        print(f"AI chose {a}.")
+
+        if a == player_input:
+            print("We are so similar! We tied")
+        elif player_input == options[0]:
+            if a == options[2]:
+                player_score += 1
+                print(f"{player_input} smashes {a}.")
+            else:
+                AI_score += 1
+                print(f"{a} covers {player_input}.")
+
+        elif player_input == options[1]:
+            if a == options[0]:
+                player_score += 1
+                print(f"{player_input} covers {a}.")
+            else:
+                AI_score += 1
+                print(f"{a} cuts {player_input}.")
+
+        elif player_input == options[2]:
+            if a == options[1]:
+                player_score += 1
+                print(f"{player_input} cuts {a}.")
+            else:
+                AI_score += 1
+                print(f"{a} smashes {player_input}. ")
+
+elif option_modes[user_mode-1] == option_modes[0]:
+    while True:
+        player_input = input(prompt).title()
+        a = get_AI_guess()
+        print(f"AI chose {a}.")
+
+        if a == player_input:
+            print("We are so similar! We tied")
+        elif player_input == options[0]:
+            if a == options[2]:
+                player_score += 1
+                print(f"{player_input} smashes {a}.")
+            else:
+                AI_score += 1
+                print(f"{a} covers {player_input}.")
+
+        elif player_input == options[1]:
+            if a == options[0]:
+                player_score += 1
+                print(f"{player_input} covers {a}.")
+            else:
+                AI_score += 1
+                print(f"{a} cuts {player_input}.")
+
+        elif player_input == options[2]:
+            if a == options[1]:
+                player_score += 1
+                print(f"{player_input} cuts {a}.")
+            else:
+                AI_score += 1
+                print(f"{a} smashes {player_input}. ")
         else:
-            AI_score += 1
-            print(f"{a} covers {player_input}.")
-
-    elif player_input == options[1]:
-        if a == options[0]:
-            player_score += 1
-            print(f"{player_input} covers {a}.")
-        else:
-            AI_score += 1
-            print(f"{a} cuts {player_input}.")
-
-    elif player_input == options[2]:
-        if a == options[1]:
-            player_score += 1
-            print(f"{player_input} cuts {a}.")
-        else:
-            AI_score += 1
-            print(f"{a} smashes {player_input}. ")
-
+            break
 
 print(f"Player got a score of {player_score}.")
 print(f"AI got a score of {AI_score}.")
@@ -45,5 +86,7 @@ print(f"AI got a score of {AI_score}.")
 if player_score > AI_score:
     print(f"Player won by {player_score - AI_score} points. Congratulations!")
 
-else:
+elif AI_score > player_score:
     print(f"AI won by {AI_score - player_score} points. Better luck next time!")
+else:
+    print("You tied with the AI!")
